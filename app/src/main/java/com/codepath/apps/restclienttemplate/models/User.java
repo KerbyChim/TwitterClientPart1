8ad92caa12f4e1c1d5d4242bf7065336ffeb8caf
KerbyChim <antoinekerby@gmail.com>
2019-08-02 22:40:12 -0400

    Adding tweet

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -2,6 +2,9 @@ package com.codepath.apps.restclienttemplate.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
+
+@Parcel
 
 public class User {
 
@@ -9,6 +12,11 @@ public class User {
     public long uid;
     public String screenName;
     public String profileImageUrl;
+
+    // empty constructor needed by the Parceler library
+    public User(){
+
+    }
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
